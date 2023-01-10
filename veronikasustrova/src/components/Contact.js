fa879682a546fba33f074245abcabd63fd197a69
vsustrov <vsustrov@uwaterloo.ca>
2023-01-09 21:21:47 -0500

    Added content to contact section

--- veronikasustrova/src/components/Contact.js
+++ veronikasustrova/src/components/Contact.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import "../css/Contact.css"
+import LinkedIn from "../assets/lk-icon.png"
+import GitHub from "../assets/gh-icon.png"
+import Facebook from "../assets/fb-icon.png"
+
 
 const Contact = () => {
   return (
@@ -7,9 +12,9 @@ const Contact = () => {
         <p>Send me an email at <a href="mailto:vsustrov@uwaterloo.ca">vsustrov@uwaterloo.ca</a>.</p>
         <p>Let's keep in touch!</p>
         <div className="socials">
-          <button>Linkedin</button>
-          <button>GitHub</button>
-          <button>Facebook</button>
+          <a href="https://www.linkedin.com/in/sustvero/"><img className="icon-img" src={LinkedIn} alt="linkedin"></img></a>
+          <a href="https://github.com/sustvero"><img className="icon-img" src={GitHub} alt="github"></img></a>
+          <a href="https://www.facebook.com/veronika.sustrova.969/"><img className="icon-img" src={Facebook} alt="facebook"></img></a>
         </div>
     </div>
   )
