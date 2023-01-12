478507f8aae5f227810dba12708fd0de2d8b44a7
vsustrov <vsustrov@uwaterloo.ca>
2023-01-11 19:46:01 -0500

    added skeleton project card

--- veronikasustrova/src/components/ProjectCard.js
+++ veronikasustrova/src/components/ProjectCard.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "../css/ProjectCard.css"
 
-const ProjectCard = () => {
+const ProjectCard = ({
+  name,
+  image,
+  description,
+  tools,
+  link
+}) => {
   return (
-    <div>ProjectCard</div>
+    <div>
+      <h5>{name}</h5>
+      <img className="project-img" src={image} alt={name}></img>
+      <h6>{tools}</h6>
+      <p>{description}</p>
+      <a href={link}><button>View Project</button></a>
+    </div>
   )
 }
 
--- veronikasustrova/src/components/Projects.js
+++ veronikasustrova/src/components/Projects.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 const Projects = () => {
   return (
     <div>
         <h2>Projects</h2>
+        <ProjectCard name="Vero's Project" tools="React" description="This is a super cool project."/>
+
     </div>
   )
 }
