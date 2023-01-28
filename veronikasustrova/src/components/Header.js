817b242d9353acdf1a7b952a5a8f92ccb157beb0
vsustrov <vsustrov@uwaterloo.ca>
2023-01-28 16:21:00 -0500

    updated styling

--- veronikasustrova/src/components/Header.js
+++ veronikasustrova/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../css/Header.css"
+import PixelHeart from "../assets/pixel-heart.png"
 
 const Header = () => {
   return (
     <header className="header">
         <div className="header-container">
             <div className="header-text">
+                <img src={PixelHeart}></img>
                 <h1>Hi! I'm Veronika.</h1>
                 <h4>I aim to build software that makes the world a safe, accessible, and inclusive place.</h4>
                 <a href="https://www.linkedin.com/in/sustvero/"><button className="connect-btn">Let's Connect</button></a>
